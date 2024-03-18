ad516d9a4535eb1f3a889f09cef7f9fef35840d7
qurror <tvs_v1@mail.ru>
2024-03-19 01:01:45 +0300

    completed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-server.listen(3000, 'localhost', () => {
-  console.log('Сервер запущен на http://localhost:3000');
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://0.0.0.0:${port}`);
 });
