adfca52c6dcf70da7288cee52f6377376b2fcac3
qurror <tvs_v1@mail.ru>
2024-03-20 01:35:25 +0300

    develop branch near cherry

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
   res.send('Hello Petushara!');
+=======
+  res.send('Hello World Petushara_NEW_FEATURE!');
+>>>>>>> da79f12 (fixing main)
 });
 
 app.listen(port, '0.0.0.0', () => {
